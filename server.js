02a867950cf278f270211e1c2ac6d17be4282753
StephenChao <mqchay001@gmail.com>
2022-09-01 17:26:46 +0100

    modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json 	modified:   server.js 	node_modules/cors/

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const newsRoutes = require('./routes/newss')
 
 const app = express()
 
-
+let cors = require("cors");
+app.use(cors());
 
 app.use(express.json({limit: '25mb'}));
 app.use(express.urlencoded({limit: '25mb'}));
