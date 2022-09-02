3408beb51155d740a1c3fce06b62b42644e08b01
StephenChao <mqchay001@gmail.com>
2022-09-02 11:49:03 +0100

    modified:   server.js

--- server.js
+++ server.js
@@ -14,11 +14,6 @@ const app = express()
 let cors = require("cors");
 app.use(cors());
 
-app.options('/:id', cors()) // enable pre-flight request for GET request
-app.get('/:id', cors(), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
 app.use(express.json({limit: '25mb'}));
 app.use(express.urlencoded({limit: '25mb'}));
 
