7d62a81cff6d9349b1338fc8c712831008003da0
StephenChao <mqchay001@gmail.com>
2022-09-02 11:55:27 +0100

    modified:   server.js

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ const app = express()
 let cors = require("cors");
 app.use(cors());
 
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+  next(); 
+})
+
+
 app.use(express.json({limit: '25mb'}));
 app.use(express.urlencoded({limit: '25mb'}));
 
